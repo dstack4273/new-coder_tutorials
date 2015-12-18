222fdf5f57d23f2ec74503d475fb1531c5e2cd1a
dstack4273 <dstackmasta27@gmail.com>
2015-12-17 22:33:28 -0500

    made some more progress on part 4

--- network/MySourceFiles/twisted/plugins/talkbackbot_plugin.py
+++ network/MySourceFiles/twisted/plugins/talkbackbot_plugin.py
@@ -10,21 +10,71 @@ from talkback.bot import TalkBackBotFactory
 from talkback.quote_picker import QuotePicker
 
 class Options(usage.Options):
+    optParameters = [
+        ['config', 'c', 'settings.ini', 'Configuration file.'],
+    ]
 
 class TalkBackBotService(Service):
+    _bot = None
 
     def __init__(self, endpoint, channel, nickname, realname, quotesFilename, triggers):
+        self._endpoint = endpoint
+        self._channel = channel
+        self._nickname = nickname
+        self._realname = realname
+        self._quotesFilename = quotesFilename
+        self._triggers = triggers
 
     def startService(self):
-        """ Constructs a client and connects to the server """
+        """Constructs a client and connects to the server."""
+        from twisted.internet import reactor
+
+        def connected(bot):
+            self._bot = bot
+
+        def failure(err):
+            log.err(err, _why='Could not connect to server.')
+            reactor.stop()
+
+        quotes = QuotePicker(self._quotesFilename)
+        client = clientFromString(reactor, self._endpoint)
+        factory = TalkBackBotFactory(
+            self._channel,
+            self._nickname,
+            self._realname,
+            quotes,
+            self._triggers,
+        )
+
+        return client.connect(factory).addCallbacks(connected, failure)
 
     def stopService(self):
-        """ Disconnect """
+        """Disconnect."""
 
+@implementer(IServiceMaker, IPlugin)
 class BotServiceMaker(object):
     tapname = "twsrs"
-    description = ""
+    description = "An IRC bot that generates quotes from notable women"
     options = Options
 
     def makeService (self, options):
-        
+        "Constructs the talkbackbot service."
+        config = ConfigParser()
+        config.read([options['config']])
+        triggers = [
+            trigger.strip()
+            for trigger
+            in config.get('talkback', 'triggers').split('\n')
+            if trigger.strip()
+        ]
+
+        return TalkBackBotService(
+            endpoint = config.get('irc', 'endpoint'),
+            channel = config.get('irc', 'channel'),
+            nickname = config.get('irc', 'nickname'),
+            realname = config.get ('irc', 'realname'),
+            quotesFilename = config.get('talkback', 'quotesFilename'),
+            triggers = triggers,
+        )
+
+serviceMaker = BotServiceMaker()
